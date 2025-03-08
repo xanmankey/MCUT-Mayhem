8c2de87df96b927153cc97f568a784421e18abb0
xanmankey <86136010+xanmankey@users.noreply.github.com>
2025-03-08 04:12:06 +0000

    updated serve scripts

--- run_mcut_mayhem_viewer.sh
+++ run_mcut_mayhem_viewer.sh
@@ -1,4 +1,4 @@
 #!/nix/store/m8xqd9hv339yxgiilbv7wvh8figqx135-system-path/bin/bash
 
 cd MCUT-Mayhem-Viewer-WebApp
-nix-shell -p "nodejs_23" --run "npm run preview"
+nix-shell -p nodejs_23 nodePackages.serve --run "serve -s dist -l 10092"
